d68dad6084ec6a63dfa629d1436d6c49fbfbc882
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-09 09:59:22 -0200

    Added  do AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,6 +23,8 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    protected $redirectPath = '/meeting';
+
     /**
      * Create a new authentication controller instance.
      *
