1b6520e3c3134c101eb341e366271534cf29a166
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-09 10:44:59 -0200

    Added var loginPath to AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,6 +24,7 @@ class AuthController extends Controller
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     protected $redirectPath = '/meeting';
+    protected $loginPath = '/';
 
     /**
      * Create a new authentication controller instance.
